91d8c2c240a8569a12ae411f2770690a41618313
Damian de Hoog <37507522+DdeHoog@users.noreply.github.com>
2025-06-09 16:50:30 +0200

    Experience.jsx, paragraphing the details and adding customs scrollbar style
    Added paragraphing throuhg \n\n and splitting the details, mapping the paragraphs die the index.
    
    Custom scrollbar added to fit style over default.

--- DdeHoog/src/components/Experience.jsx
+++ DdeHoog/src/components/Experience.jsx
@@ -6,42 +6,42 @@ const experiences = [
     name: "Military officer",
     title: "Military Officer program, Dutch Royal Military Academy",
     duration: "Jun 2022 - Mar 2023",
-    details: "Military officer educational program, containing intensive physical training, leadership training and, designing and managing tasks and squad-tasks. Personal development of skills and the self under extreme conditions combined with a focus on becoming a good leader and manager."
+    details: "Military officer educational program, containing intensive physical and leadership training as well as designing and managing tasks and squad-tasks. \n\n \n\nBasic training of skills such as; weapons, weapon maintenance and medical. Development of the self under extreme conditions combined with a focus on becoming a good leader and manager. Basic weapons and medical training."
   },
   {
     id: 2,
     name: "Tokyo research",
     title: "Research exchange program, Tokyo, Japan.",
     duration: "Aug 2021 - Mar 2022",
-    details: "Remote work under assistance of the lab at Shibaura Institute of Technology. Solo work on research, planning and development of a working protoype. Researching, planning and development of an EMG controlled robotic hand. Prototype developed and deliverd."
+    details: "Remote work under assistance of the lab at Shibaura Institute of Technology (SIT). \n\n \n\nResearch, planning and development of a cypress microcontroller based EMG-controlled robotic hand. Prototype developed, tested and delivered as product for SIT."
   },
   {
     id: 3,
     name: "CERN project",
     title: "CERN project coordinator, AUAS.",
     duration: "Feb 2020 - Feb 2021",
-    details: "Project coordinator for the Jiskefet project for CERN, coordinating a team of 7. As well as software developer using Javascript, Mithril, Express, Sequelize (ORM), Pupperteer and Jira. Developing a web application for CERN to log meta-data pertaining to the large hadron collider experiments."
+    details: "Project manager and coordinator for the Jiskefet project for CERN, an Amsterdam University of Applied Sciences(AUAS) project. \n\nCoordinating a team of 7, as well as software development using Javascript, Mithril, Express, Sequelize (ORM), Pupperteer and Jira. \n\n \n\nDevelopment of a web application for CERN to log meta-data pertaining to the large hadron collider experiments."
   },
   {
     id: 4,
     name: "Tokyo robotics",
     title: "Studying robotics in Tokyo, Japan.",
     duration: "Sep 2019 - Mar 2020",
-    details: "Studying at the Shibaura Institute of Technology in Tokyo, Japan. Courses contain elements of robotics, electrical circuits, 3D modeling, testing and designing programming language processors. This semester supported personal development through experiences far outside my comfort-zone; new languages, locations and methods of learning as well as further my specializations."
+    details: "Studying at the Shibaura Institute of Technology in Tokyo, Japan. \n\nCourses contain elements of robotics, electrical circuits, 3D modeling, testing and designing programming language processors. \n\n \n\nThis semester supported personal development through experiences far outside my comfort-zone; new languages, locations and methods of learning as well as furthering my specializations in software and hardware combinatory projects and assignments."
   },
   {
     id: 5,
     name: "Startups",
     title: "Creative startups",
     duration: "Feb 2019 - June 2019",
-    details: "Creating, developing and validating our own startup idea according to the lean startup methodology. Scrum master in a team of 5. Developing a startup idea, pivoting around key features ofthe idea using various experiments and validation methods"
+    details: "Creating, developing and validating our startup ideas according to the lean startup methodology. Using market research and customer validation to verify startup ideas. \n\n \n\nScrum master in a team of 5. \n\nDeveloping a startup idea, pivoting around key features ofthe idea using various experiments and validation methods"
   },
   {
     id: 6,
     name: "AUAS robotics",
     title: "Rescue on wheels",
     duration: "Sep 2018 - Feb 2019",
-    details: "Software and hardware development - Python/Java android developer, hardware assembly and scrum master in a team of 4. Developing a Raspberry pi 3B mobile robot with live camera feed which can be remote controlled from a mobile device or windows device."
+    details: "Developing a Raspberry pi 3B mobile robot with live camera feed which can be remote controlled from a mobile or windows device.\n\n \n\nSoftware and hardware development using Java for android, as well as hardware assembly and Python for the robot on wheels. \n\nScrum master in a team of 4 and product owner."
   }
 ];
 
@@ -78,7 +78,7 @@ const Experience = () => {
       <div className="text-base h-full w-[65%] md:w-[65%] flex flex-col min-w-0 overflow-hidden"> {/* Details section block */ }
         <div className="bg-black h-full w-full p-0 rounded-md flex-1 min-h-0 overflow-auto">{/*Right; black background */}
           {selectedExperience ? (
-            <div className="w-full h-full break-words whitespace-pre-wrap overflow-y-auto p-1">{/*Right; div containing the elements */}
+            <div className="w-full h-full break-words whitespace-pre-wrap overflow-y-auto p-1 custom-scrollbar">{/*Right; div containing the elements */}
               {/*Right; title */}
               <h2 className="text-[0.5rem] font-bold mb-2 break-words sm:text-[0.8rem] 2xl:text-base  md:text-[0.9rem] md:text-base">
                 {selectedExperience.title}</h2>
@@ -88,8 +88,14 @@ const Experience = () => {
                 {selectedExperience.duration}</p>
 
               {/*Right; detail text*/}
-              <p className="text-[0.4rem] sm:text-[0.5rem] md:text-[0.6rem] xl:text-base whitespace-pre-wrap break-words">
-                {selectedExperience.details}</p>
+              {selectedExperience.details.split('\n\n').map((para, index) => (
+                <p 
+                key={index} 
+                className="text-[0.4rem] sm:text-[0.5rem] md:text-[0.6rem] xl:text-base whitespace-pre-wrap break-words">
+                  {para}
+                </p>
+              ))}
+              
             </div>
           ) : ( 
             <p className="text-white-500 italic"></p>

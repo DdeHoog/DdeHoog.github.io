467a7b88c7a3f8dd7deb2ce48718b65e2fc96108
Damian de Hoog <37507522+DdeHoog@users.noreply.github.com>
2025-06-09 16:53:19 +0200

    Navbar contact->about rename

--- DdeHoog/src/components/NavBar.jsx
+++ DdeHoog/src/components/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = ({ onLogoClick }) => {
                 <ul className="navbar-right">
                     <li><a href="#portfolio">Portfolio</a></li>
                     <li><a href="#experience">Experience</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#about">About</a></li>
                 </ul>
             </div>
         </nav>
